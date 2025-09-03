243dae674f3a22256b3e64c6fa7de34a34773641
Nicholas Leal <nicholasgdml12@outlook.com>
2025-09-03 19:18:54 -0300

    refazendo ex003 kkkkkkk

--- secao10_exercicioVetor/src/exercicos/ex003.java
+++ secao10_exercicioVetor/src/exercicos/ex003.java
@@ -5,24 +5,6 @@ import java.util.Scanner;
 public class ex003 {
 
 	public static void main(String[] args) {
-		// Alturas
 		
-		Scanner sc = new Scanner(System.in);
-		
-		System.out.print("Quantas pessoas serao digitadas? ");
-		int numeroPessoas = sc.nextInt();
-		
-		
-		for (int i = 0; i < numeroPessoas; i++) {
-			System.out.printf("Dados %dª pessoa:\n", i);
-			System.out.print("Nome: ");
-			String nome = sc.nextLine();
-			System.out.print("Idade: ");
-			int idade = sc.nextInt();
-			System.out.print("Altura: ");
-			Double altura = sc.nextDouble();
-		}
-
 	}
-
 }
