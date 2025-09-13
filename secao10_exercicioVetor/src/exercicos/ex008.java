9544c9afdeb65ce9622caecf9b3d4a78004e5207
Nicholas Leal <nicholasgdml12@outlook.com>
2025-09-12 21:59:02 -0300

    ex008 feito

--- secao10_exercicioVetor/src/exercicos/ex008.java
+++ secao10_exercicioVetor/src/exercicos/ex008.java
@@ -1,11 +1,42 @@
 package exercicos;
 
+import java.util.Scanner;
+
 public class ex008 {
 
 	public static void main(String[] args) {
 		// media_pares
 
-		sc
+		Scanner sc = new Scanner(System.in);
+		int tamanhoVetor;
+		int somaPares = 0;
+		int mediaPares = 0;
+		int quantidadeNumPar = 0;
+		
+		System.out.print("Quantos elementos vai ter o vator?: ");
+		tamanhoVetor = sc.nextInt();
+		
+		int[] vetor = new int[tamanhoVetor];
+		
+		for (int i = 0; i < tamanhoVetor; i++) {
+			System.out.print("Digite um numero: ");
+			vetor[i] = sc.nextInt();
+		}
+		
+		for (int i = 0; i < tamanhoVetor; i++) {
+			if (vetor[i] % 2 == 0) {
+				somaPares += vetor[i];
+				quantidadeNumPar += 1;
+			}
+		}
+		
+		
+		if (somaPares != 0) {
+			mediaPares = somaPares / quantidadeNumPar;
+			System.out.println("MEDIA DOS PARES = " + mediaPares);
+		} else {
+			System.out.println("NENHUM NUMERO PAR");
+		}
 	}
 
 }
