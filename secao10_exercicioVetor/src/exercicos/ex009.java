b530e6899040deb46621b80ac21dc480381ec930
Nicholas Leal <nicholasgdml12@outlook.com>
2025-09-14 21:45:20 -0300

    ex 009 finalizado de vdd

---
+++ secao10_exercicioVetor/src/exercicos/ex009.java
@@ -0,0 +1,40 @@
+package exercicos;
+
+import java.util.Scanner;
+
+public class ex009 {
+
+	public static void main(String[] args) {
+		// mais_velho
+			
+		Scanner sc = new Scanner(System.in);
+		String maiorIdadeNome = "";
+		int maisVelho;
+		int tamanhoVetor;
+		int maiorIdade = 0;
+		
+		System.out.print("Quantas pessoas voce vai digitar: ");
+		tamanhoVetor = sc.nextInt();
+		sc.nextLine();
+		String[] nomes = new String[tamanhoVetor];
+		int[] idades = new int[tamanhoVetor];
+		
+		for (int i = 0; i < tamanhoVetor; i++) {
+			System.out.printf("Dados da %dª pessoa:\n", i+1);
+			System.out.print("Nome: ");
+			nomes[i] = sc.nextLine();
+			System.out.print("Idade: ");
+			idades[i] = sc.nextInt();
+			sc.nextLine();
+		}
+		
+		for (int i = 0; i < tamanhoVetor; i++) {
+			if (idades[i] > maiorIdade) {
+				maiorIdade = idades[i];
+				maiorIdadeNome = nomes[i];
+			}
+		}
+		
+		System.out.println("PESSOA MAIS VELHA: " + maiorIdadeNome);
+	} 
+}
